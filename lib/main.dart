6d34fe0e63597292c4e6129c3be181ef47c20dfd
ghostwatchmecode <prayatna.rajkarnikar@gmail.com>
2024-08-27 14:00:24 +0545

    Done

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Profile',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.grey[600],
+        ),
+        backgroundColor: Colors.grey[200],
+        body: const ProfileCard(),
+      ),
+    );
+  }
+}
+
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        child: Card(
+          elevation: 4,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const CircleAvatar(
+                  radius: 80,
+                  backgroundImage: AssetImage('assets/images/picture.jpg'),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Text(
+                  "Prayatna Rajkarnikar",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "Mobile App Developer",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.grey[600],
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const Text(
+                  "✉️prayatna.rajkarnikar@gmail.com",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const Text(
+                  "Always eager to learn new technologies and improve my coding skills",
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text("Contact Me ✌️"))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
